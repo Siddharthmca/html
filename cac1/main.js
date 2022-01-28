8dbb71fcc9ff372c0c07835d6240a8c4dd75ef12
Joseph K Iype <40719207+Joseph-k-iype@users.noreply.github.com>
2022-01-28 22:45:09 +0530

    new

--- cac1/main.js
+++ cac1/main.js
@@ -1,62 +1,10 @@
-// var myInput = document.getElementById("psw");
-// var letter = document.getElementById("letter");
-// var capital = document.getElementById("capital");
-// var number = document.getElementById("number");
-// var length = document.getElementById("length");
-
-// // When the user clicks on the password field, show the message box
-// myInput.onfocus = function() {
-//   document.getElementById("message").style.display = "block";
-// }
-
-// // When the user clicks outside of the password field, hide the message box
-// myInput.onblur = function() {
-//   document.getElementById("message").style.display = "none";
-// }
-
-// // When the user starts to type something inside the password field
-// myInput.onkeyup = function() {
-//   // Validate lowercase letters
-//   var lowerCaseLetters = /[a-z]/g;
-//   if(myInput.value.match(lowerCaseLetters)) {  
-//     letter.classList.remove("invalid");
-//     letter.classList.add("valid");
-//   } else {
-//     letter.classList.remove("valid");
-//     letter.classList.add("invalid");
-//   }
-  
-//   // Validate capital letters
-//   var upperCaseLetters = /[A-Z]/g;
-//   if(myInput.value.match(upperCaseLetters)) {  
-//     capital.classList.remove("invalid");
-//     capital.classList.add("valid");
-//   } else {
-//     capital.classList.remove("valid");
-//     capital.classList.add("invalid");
-//   }
-
-//   // Validate numbers
-//   var numbers = /[0-9]/g;
-//   if(myInput.value.match(numbers)) {  
-//     number.classList.remove("invalid");
-//     number.classList.add("valid");
-//   } else {
-//     number.classList.remove("valid");
-//     number.classList.add("invalid");
-//   }
-  
-//   // Validate length
-//   if(myInput.value.length >= 8) {
-//     length.classList.remove("invalid");
-//     length.classList.add("valid");
-//   } else {
-//     length.classList.remove("valid");
-//     length.classList.add("invalid");
-//   }
-// }
-
-
+var specialKeys = new Array();
+        specialKeys.push(8); //Backspace
+        function IsAlpha(e) {
+            var keyCode = e.which ? e.which : e.keyCode
+            var ret = ((keyCode >= 97 && keyCode <= 122) || specialKeys.indexOf(keyCode) != -1);
+            return ret;
+        }
 
 function test_str() {
     var res;
@@ -81,12 +29,3 @@ function test_str() {
 function reset() {
     document.getElementById("feedback-form").reset();
   }
-// function username_str() {
-//     var res;
-//     var str = 
-//     document.getElementById("t3").value;
-//     if(str.match(/^([a-z0-9_\.]+)@(([a-z0-9]+)\.){2}([a-z]{2,3})$/))
-//         console.log("Valid email")
-//     else
-//         alert("Invalid email")
-// }

bf5f5adec2053144b45277b99ffdcd71aa2a676b
Joseph K Iype <40719207+Joseph-k-iype@users.noreply.github.com>
2022-02-09 09:20:09 +0530

    new

--- lab8/main.js
+++ lab8/main.js
@@ -1,86 +1,47 @@
-var app = angular.module("mainApp", []);
+var sample=angular.module("sample", []);
+sample.controller("myctrl", function($scope, $http)
+{
+    $http.get('sample.json')
+    .success(function(response)
+    {
+        $scope.entry=response.data;
+    });
+});
 
-// app.filter("myFilter", function(){
-//     return function(input, option){
-//         if(isNaN(option) || option == ""){
-//             return input;
-//         }
-//         else 
-//         {
-//             return input.substring(0, option).toUpperCase();
-//         }
-//     }
-// });
-
-app.filter("myFilter", function () {
-    return function (input, option) {
-        var output = [];
-        if (isNaN(option) || option == "") {
-            return input;
-        }
-        else {
-            angular.forEach(input, function (value, key) {
-                if (value.id > option) {
-                    output.push(value);
-                }
-            });
-            return output;
-        }
+sample.filter("toUpperCase", function () {
+    return function (input)
+    {
+        var output = "";       
+        output = input.toUpperCase();
+        return output;
     }
 });
-app.controller("UserController", function ($scope, $http) {
 
-
-    $http.get('sample.json').success(function (response) {
-        $scope.users = response;
-    });
-
-
+sample.filter("americanConvertion", function () {
+    return function (input)
+    {
+        var rupee ;       
+        rupee = input / 75;
+        return rupee;
+    }
 });
 
-$scope.pagination = function () {
-
-    $scope.curPage = 1,
-        $scope.itemsPerPage = 3,
-        $scope.maxSize = 5;
-
-    this.items = users;
-
-
-    $scope.numOfPages = function () {
-        return Math.ceil(users.length / $scope.itemsPerPage);
-
-    };
-
-    $scope.$watch('curPage + numPerPage', function () {
-        var begin = (($scope.curPage - 1) * $scope.itemsPerPage),
-            end = begin + $scope.itemsPerPage;
-
-        $scope.filteredItems = users.slice(begin, end);
-    });
-
-}
-
-// app.controller('UserController', function ($scope) {
-
-//     $scope.curPage = 1,
-//         $scope.itemsPerPage = 3,
-//         $scope.maxSize = 5;
-
-//     this.items = users;
-
-
-//     $scope.numOfPages = function () {
-//         return Math.ceil(users.length / $scope.itemsPerPage);
-
-//     };
-
-//     $scope.$watch('curPage + numPerPage', function () {
-//         var begin = (($scope.curPage - 1) * $scope.itemsPerPage),
-//             end = begin + $scope.itemsPerPage;
-
-//         $scope.filteredItems = users.slice(begin, end);
-//     });
-// });
-
-
+sample.filter('salaryGreaterThan', function () {
+ 
+    return function (input, Salary) {
+        var output = [];
+        if (isNaN(Salary)) {
+ 
+            output = input;
+        }
+        else {
+            angular.forEach(input, function (item) {
+ 
+                if (item.Salary > Salary) {
+                    output.push(item)
+                }
+            });
+        }
+        return output;
+    }
+})

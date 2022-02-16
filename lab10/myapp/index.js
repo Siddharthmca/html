d9800ab62bf6051ff261e5f934408a95d39294b2
Joseph K Iype <40719207+Joseph-k-iype@users.noreply.github.com>
2022-02-16 11:48:23 +0530

    new

--- lab10/myapp/index.js
+++ lab10/myapp/index.js
@@ -1,8 +1,10 @@
-'use strict';
+
+const request = require('request');
 
 const fs = require('fs');
 const express = require('express');
 const app = express();
+
 
 
 app.get('/', (req, res) => {
@@ -13,38 +15,42 @@ app.get('/', (req, res) => {
 var http = require('http');
 
 // Create a server object
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
 
-    // http header
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function (err, data) {
+        // http header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    var url = req.url;
+        var url = req.url;
 
-    if (url === '/database.json') {
-        console.log("reading again");
-        const jsonData = require('./database.json');
-        console.log(jsonData);
-        res.write(' data loaded');
-        res.end();
+        if (url === '/database.json') {
+            console.log("reading again");
+            const jsonData = require('./database.json');
+            console.log(jsonData);
+            res.write(' data loaded');
+            res.write(data);
+            res.end();
 
-    } else if (url === '/about') {
-        console.log("welcome to COFFEE HOUSE COMPANY ");
-        res.write(' Welcome to COFFEE HOUSE COMPANY');
-        res.end();
-    
-    }
-    else if (url === '/contact') {
-        console.log("welcome to our app ");
-        res.write(' Welcome to COFFEE HOUSE COMPANY');
-        res.end();
-    } 
-    else {
-      //  res.writeHead(200);
-        res.write('Server loaded.');
-        res.end();
+        } else if (url === '/about') {
+            console.log("welcome to COFFEE HOUSE COMPANY ");
+            res.write(' Welcome to COFFEE HOUSE COMPANY');
+            res.write(data);
+            res.end();
 
-    }
-}).listen(3001, function() {
+        }
+        else if (url === '/contact') {
+            console.log("welcome to our app ");
+            res.write(' Welcome to COFFEE HOUSE COMPANY');
+            res.end();
+        }
+        else {
+            //  res.writeHead(200);
+            res.write('Server loaded.');
+            res.end();
+
+        }
+    });
+}).listen(3001, function () {
 
     // The server object listens on port 3000
     console.log("server start at port 8081");
@@ -56,59 +62,76 @@ let mechanic = JSON.parse(rawdata);
 console.log(mechanic); 
 */
 //
-let managers = {
-    "managers":[
-        {
-            "name": "Akshaya",
-            "email": "akshayagmail.com",
-            "phone": "9148827639",
-            "role": "Executive",
-            "performance": 0.92,
-            "salary": 20000
-        },
-        {
-            "name": "Pavan",
-            "email": "pavan@gmail.com",
-            "phone": "9100997798",
-            "role": "Intern",
-            "performance": 0.65,
-            "salary": 12000
-        },
-        {
-            "name": "Ashwin",
-            "email": "ashwin@gmail.com",
-            "phone": "9800657341",
-            "role": "Executive",
-            "performance": 0.78,
-            "salary": 23000
-        },
-        {
-            "name": "Vaishnavi",
-            "email": "vaishnavi@gmail.com",
-            "phone": "8935667832",
-            "role": "HR",
-            "performance": 0.91,
-            "salary": 21300
-        },
-        {
-            "name": "Pooja",
-            "email": "pooja@gmail.com",
-            "phone": "91009902",
-            "role": "Intern",
-            "performance": 0.87,
-            "salary": 12000
-        },
-        {
-            "name": "Anisha",
-            "email": "anisha@gmail.com",
-            "phone": "9167990012",
-            "role": "Intern",
-            "performance": 0.78,
-            "salary": 10000
-        }
-]
 
-};
+// let managers = {
+//     "managers": [
+//         {
+//             "name": "Akshaya",
+//             "email": "akshayagmail.com",
+//             "phone": "9148827639",
+//             "role": "Executive",
+//             "performance": 0.92,
+//             "salary": 20000
+//         },
+//         {
+//             "name": "Pavan",
+//             "email": "pavan@gmail.com",
+//             "phone": "9100997798",
+//             "role": "Intern",
+//             "performance": 0.65,
+//             "salary": 12000
+//         },
+//         {
+//             "name": "Ashwin",
+//             "email": "ashwin@gmail.com",
+//             "phone": "9800657341",
+//             "role": "Executive",
+//             "performance": 0.78,
+//             "salary": 23000
+//         },
+//         {
+//             "name": "Vaishnavi",
+//             "email": "vaishnavi@gmail.com",
+//             "phone": "8935667832",
+//             "role": "HR",
+//             "performance": 0.91,
+//             "salary": 21300
+//         },
+//         {
+//             "name": "Pooja",
+//             "email": "pooja@gmail.com",
+//             "phone": "91009902",
+//             "role": "Intern",
+//             "performance": 0.87,
+//             "salary": 12000
+//         },
+//         {
+//             "name": "Anisha",
+//             "email": "anisha@gmail.com",
+//             "phone": "9167990012",
+//             "role": "Intern",
+//             "performance": 0.78,
+//             "salary": 10000
+//         }
+//     ]
+
+// };
+
+let url = 'https://github.com/Joseph-k-iype/html/blob/master/lab10/myapp/database.json';
+
+let options = {json: true};
+
+
+
+var managers = request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+        // do something with JSON, using the 'body' variable
+    };
+});
 
 let data = JSON.stringify(managers);
 
